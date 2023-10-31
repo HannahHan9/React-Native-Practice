55666b8d2f9739d38a563054b2ad76e8db0c6303
HannahHan9 <h.han9204@gmail.com>
2023-10-31 16:30:24 +0000

    add event handlers

--- App.js
+++ App.js
@@ -1,17 +1,35 @@
 import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import { useState } from "react";
 
 export default function App() {
+        const [enteredGoalText, setEnteredGoalText] = useState("");
+        const [courseGoals, setCourseGoals] = useState([]);
+        const goalInputHandler = (enteredText) => {
+                setEnteredGoalText(enteredText);
+        };
+        const addGoalHandler = () => {
+                setCourseGoals((currentCourseGoals) => [
+                        ...currentCourseGoals,
+                        enteredGoalText,
+                ]);
+        };
         return (
                 <View style={styles.appContainer}>
                         <View style={styles.inputContainer}>
                                 <TextInput
                                         style={styles.textInput}
                                         placeholder="Your course goal!"
+                                        onChangeText={goalInputHandler}
                                 />
-                                <Button title="Add Goal" />
+                                <Button
+                                        title="Add Goal"
+                                        onPress={addGoalHandler}
+                                />
                         </View>
-                        <View>
-                                <Text>List of goals...</Text>
+                        <View style={styles.goalsContainer}>
+                                {courseGoals.map((goal) => (
+                                        <Text key={goal}>{goal}</Text>
+                                ))}
                         </View>
                 </View>
         );
@@ -19,17 +37,27 @@ export default function App() {
 
 const styles = StyleSheet.create({
         appContainer: {
-                padding: 50,
+                flex: 1,
+                paddingTop: 50,
+                paddingHorizontal: 16,
         },
         inputContainer: {
+                flex: 1,
                 flexDirection: "row",
                 justifyContent: "space-between",
+                alignItems: "center",
+                marginBottom: 24,
+                borderBottomWidth: 1,
+                borderBottomColor: "#cccccc",
         },
         textInput: {
                 borderWidth: 1,
                 borderColor: "#cccccc",
-                width: "80%",
+                width: "70%",
                 marginRight: 8,
                 padding: 8,
+        },
+        goalsContainer: {
+                flex: 5,
         },
 });
