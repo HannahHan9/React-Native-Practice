6a7d8212ab42c9cc7e38932aa035e524e6dc9a5a
HannahHan9 <h.han9204@gmail.com>
2023-11-01 17:50:43 +0000

    add onDelete function

--- App.js
+++ App.js
@@ -12,6 +12,12 @@ export default function App() {
       { text: enteredGoalText, id: Math.random().toString() },
     ]);
   };
+
+  const deleteGoalHandler = (id) => {
+    setCourseGoals((currentCourseGoals) => {
+      return currentCourseGoals.filter((goal) => goal.id !== id);
+    });
+  };
   return (
     <View style={styles.appContainer}>
       <GoalInput onAddGoal={addGoalHandler} />
@@ -19,7 +25,13 @@ export default function App() {
         <FlatList
           data={courseGoals}
           renderItem={(itemData) => {
-            return <GoalItem text={itemData.item.text} />;
+            return (
+              <GoalItem
+                text={itemData.item.text}
+                onDeleteItem={deleteGoalHandler}
+                id={itemData.item.id}
+              />
+            );
           }}
           keyExtractor={(item, index) => {
             return item.id;
